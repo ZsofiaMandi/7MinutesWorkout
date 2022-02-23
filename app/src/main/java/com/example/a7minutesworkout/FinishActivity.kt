8edd3c91c929b8b303ad1ad19d42a8a9508cc3b5
Zsófia <mandi.zsofia@gmail.com>
2022-02-23 18:42:14 +0100

    Adding Room database

--- app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/FinishActivity.kt
@@ -3,7 +3,12 @@ package com.example.a7minutesworkout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -29,6 +34,25 @@ class FinishActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        val historyDao = (application as WorkoutApp).db.historyDao()
+        addDataToDatabase(historyDao)
+    }
+
+    private fun addDataToDatabase(historyDao: HistoryDao){
+
+        val mCalendar = Calendar.getInstance()
+        val dateTime = mCalendar.time
+        Log.e("Date: ", "" +dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Date: ", "" +date)
+
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
+            Log.e("Date: ", "Added...")
         }
     }
 }
