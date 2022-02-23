0aa1aeb231954fc229d7a19724ac5224a26ff502
Zsófia <mandi.zsofia@gmail.com>
2022-02-23 18:59:31 +0100

    Fetch dates from db and log them

--- app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/HistoryActivity.kt
@@ -2,7 +2,11 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -26,6 +30,20 @@ class HistoryActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        val historyDao = (application as WorkoutApp).db.historyDao()
 
+        lifecycleScope.launch {
+            historyDao.fetchAllWorkouts().collect{
+                val workoutList = ArrayList(it)
+                Log.e("Fetched dates: ", "" +it)
+            }
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        
+        binding = null
     }
 }
